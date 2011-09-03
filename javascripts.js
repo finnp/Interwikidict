486775f03d8f1c65eef268ee3f8016516a57aa43
Finn Pauls <derfinn+github@gmail.com>
2011-09-03 21:51:38 +0200

    update crapyp code

--- javascripts.js
+++ javascripts.js
@@ -1,3 +1,15 @@
+var drop_x;
+var drop_y;
+var source_start_x = new Array('startlang', 'targetlang');
+var source_start_y = new Array('startlang', 'targetlang');
+var active_element = new Array('startlang', 'targetlang');
+active_element['startlang'] = 'lang_en';
+active_element['targetlang'] = 'lang_de';
+source_start_x['startlang'] = '8';
+source_start_x['targetlang'] = '8';
+source_start_y['startlang'] = '27';
+source_start_y['targetlang'] = '8';
+ 
 function translate(word, startlang, endlang) {
     new Ajax.Request("http://finnpauls.de/interwikidict/query.php?lang1="+startlang+"&lang2="+endlang+"&word="+word, {
         onLoading: function(){
@@ -13,14 +25,15 @@ function translate(word, startlang, endlang) {
 }
 window.onload=function()
 {
+    var draggable_options = { revert: false, onEnd: function (element) {    onEndAction(element);    }, onStart: function(element) {drop_x = element.element.offsetLeft; drop_y = element.element.offsetTop;} };
     //Draggables
-     new Draggable('lang_de', { revert: true, onEnd: function () {    onEndAction();    } });
-     new Draggable('lang_en', { revert: true, onEnd: function () {    onEndAction();    } });
-     new Draggable('lang_fr', { revert: true, onEnd: function () {    onEndAction();    } });
-     new Draggable('lang_it', { revert: true, onEnd: function () {    onEndAction();    } });
-     new Draggable('lang_pl', { revert: true, onEnd: function () {    onEndAction();    } });
-     new Draggable('lang_es', { revert: true, onEnd: function () {    onEndAction();    } });
-     new Draggable('lang_ja', { revert: true, onEnd: function () {    onEndAction();    } });
+     new Draggable('lang_de', draggable_options);     
+     new Draggable('lang_en', draggable_options);
+     new Draggable('lang_fr', draggable_options);
+     new Draggable('lang_it', draggable_options);
+     new Draggable('lang_pl', draggable_options);
+     new Draggable('lang_es', draggable_options);
+     new Draggable('lang_ja', draggable_options);
      //Droppables
     Droppables.add('outputbox', { 
     accept: 'lang',
@@ -42,14 +55,44 @@ window.onload=function()
   });
 }
 
+
+
 function onDropAction (element, lang, box) {
     langcode = element.id.split('_');
     
     if(!(langcode[1] == $('startlang').value || langcode[1] == $('targetlang').value)) {
     $(lang).value = langcode[1]; 
-    $(lang+'info').innerHTML = element.innerHTML;
     } 
     translate($('translateinput').value, $('startlang').value, $('targetlang').value);
+    if(element.id!=active_element[lang] && (element.id == active_element['startlang'] || element.id == active_element['startlang'])) {
+     if(element.id == active_element['startlang']) {
+     new Effect.Move($(active_element[lang]), { x: 400, y: 125, mode: 'absolute' });
+     } else {
+     new Effect.Move($(active_element[lang]), { x: 600, y: 125, mode: 'absolute' });
+     }
+    } else {
+     new Effect.Move($(active_element[lang]), { x: source_start_x[lang], y: source_start_y[lang], mode: 'absolute' }); }
+    if(active_element['startlang'] == element.id) {
+    source_start_x[lang] = source_start_x['startlang'];
+    source_start_y[lang] = source_start_y['startlang'];
+    } else if(active_element['targetlang'] == element.id) {
+    source_start_x[lang] = source_start_x['targetlang'];
+    source_start_y[lang] = source_start_y['targetlang'];  
+    } else {
+    source_start_x[lang] = drop_x;
+    source_start_y[lang] = drop_y;
+    }
+    active_element[lang] = element.id;
+    if(lang=='startlang'){
+    drop_x = 400;
+    drop_y = 125;
+    } else {
+    drop_x = 600;
+    drop_y = 125;  
+    }
+    
+
+   
 }
 
 function onHoverAction (element, lang, box) {
@@ -59,7 +102,8 @@ function onHoverAction (element, lang, box) {
  }
 }
 
-function onEndAction() {
+function onEndAction(element) {
+  new Effect.Move(element.element, { x: drop_x, y: drop_y, mode: 'absolute' });
   $('inputbox').setAttribute("class", "input-box");  
   $('outputbox').setAttribute("class", "output-box");  
 }

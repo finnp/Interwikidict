ee70a3fb6c236938848afc15b9e70ec787c8770c
Finn Pauls <derfinn+github@gmail.com>
2011-09-03 23:42:47 +0200

    made javascript code a bit more handy, still crappy code

--- javascripts.js
+++ javascripts.js
@@ -1,17 +1,57 @@
 var drop_x;
 var drop_y;
-var source_start_x = new Array('startlang', 'targetlang');
-var source_start_y = new Array('startlang', 'targetlang');
-var active_element = new Array('startlang', 'targetlang');
-active_element['startlang'] = 'lang_en';
-active_element['targetlang'] = 'lang_de';
-source_start_x['startlang'] = '8';
-source_start_x['targetlang'] = '8';
-source_start_y['startlang'] = '27';
-source_start_y['targetlang'] = '8';
- 
-function translate(word, startlang, endlang) {
-    new Ajax.Request("http://finnpauls.de/interwikidict/query.php?lang1="+startlang+"&lang2="+endlang+"&word="+word, {
+var lang = {
+from: {idleposition: {x: 8, y: 27}, iso: 'en'},
+to: {idleposition: {x: 8, y: 8}, iso: 'de'}
+}
+
+function onDropAction (element, lang_target, box) {
+    if(element.dataset.iso!=lang_target.iso && (element.dataset.iso == lang.from.iso || element.dataset.iso == lang.to.iso)) {
+     if(element.dataset.iso == lang.from.iso) {
+     new Effect.Move($("lang_"+lang_target.iso), { x: 400, y: 125, mode: 'absolute' });
+     } else {
+     new Effect.Move($("lang_"+lang_target.iso), { x: 600, y: 125, mode: 'absolute' });
+     }
+    } else {
+     new Effect.Move($("lang_"+lang_target.iso), { x: lang_target.idleposition.x, y: lang_target.idleposition.y, mode: 'absolute' }); }
+    if(lang.from.iso == element.id) {
+    lang_target.idleposition.x = lang.from.idleposition.x;
+    lang_target.idleposition.y = lang.from.idleposition.y;
+    } else if(lang.to.iso == element.id) {
+    lang_target.idleposition.x = lang.to.idleposition.x;
+    lang_target.idleposition.y = lang.to.idleposition.y;  
+    } else {
+    lang_target.idleposition.x = drop_x;
+    lang_target.idleposition.y = drop_y;
+    }
+    lang_target.iso = element.dataset.iso;
+    if(lang_target.iso ==lang.from.iso){
+    drop_x = 400;
+    drop_y = 125;
+    } else {
+    drop_x = 600;
+    drop_y = 125;  
+    }
+        translate(); 
+
+   
+}
+
+function onHoverAction (element, lang_target, box) {
+  if(element.dataset.iso == lang_target.iso) {
+   $(box).setAttribute("class", "reject");   
+ }
+}
+
+function onEndAction(element) {
+  new Effect.Move(element.element, { x: drop_x, y: drop_y, mode: 'absolute' });
+  $('inputbox').setAttribute("class", "input-box");  
+  $('outputbox').setAttribute("class", "output-box");  
+}
+
+function translate() {
+
+    new Ajax.Request("http://finnpauls.de/interwikidict/query.php?lang1="+lang.from.iso+"&lang2="+lang.to.iso+"&word="+$('translateinput').value, {
         onLoading: function(){
           //loading
         },
@@ -23,9 +63,10 @@ function translate(word, startlang, endlang) {
        }
 	});
 }
+
 window.onload=function()
 {
-    var draggable_options = { revert: false, onEnd: function (element) {    onEndAction(element);    }, onStart: function(element) {drop_x = element.element.offsetLeft; drop_y = element.element.offsetTop;} };
+    var draggable_options = { onEnd: function (element) {    onEndAction(element);    }, onStart: function(element) {drop_x = element.element.offsetLeft; drop_y = element.element.offsetTop;} };
     //Draggables
      new Draggable('lang_de', draggable_options);     
      new Draggable('lang_en', draggable_options);
@@ -39,71 +80,18 @@ window.onload=function()
     accept: 'lang',
     hoverclass: 'accept',
     onDrop: function(element) { 
-    onDropAction(element, 'targetlang', 'outputbox');   },
+    onDropAction(element, lang.to, 'outputbox');   },
     onHover: function(element) {
-    onHoverAction(element, 'startlang', 'outputbox');
+    onHoverAction(element, lang.from, 'outputbox');
     }
     });
     Droppables.add('translateinput', { 
     accept: 'lang',
     hoverclass: 'accept',
     onDrop: function(element) { 
-    onDropAction(element, 'startlang', 'outputbox');   },    
+    onDropAction(element, lang.from, 'outputbox');   },    
     onHover: function(element) {
-    onHoverAction(element, 'targetlang', 'inputbox');
+    onHoverAction(element, lang.to, 'inputbox');
     }
   });
-}
-
-
-
-function onDropAction (element, lang, box) {
-    langcode = element.id.split('_');
-    
-    if(!(langcode[1] == $('startlang').value || langcode[1] == $('targetlang').value)) {
-    $(lang).value = langcode[1]; 
-    } 
-    translate($('translateinput').value, $('startlang').value, $('targetlang').value);
-    if(element.id!=active_element[lang] && (element.id == active_element['startlang'] || element.id == active_element['startlang'])) {
-     if(element.id == active_element['startlang']) {
-     new Effect.Move($(active_element[lang]), { x: 400, y: 125, mode: 'absolute' });
-     } else {
-     new Effect.Move($(active_element[lang]), { x: 600, y: 125, mode: 'absolute' });
-     }
-    } else {
-     new Effect.Move($(active_element[lang]), { x: source_start_x[lang], y: source_start_y[lang], mode: 'absolute' }); }
-    if(active_element['startlang'] == element.id) {
-    source_start_x[lang] = source_start_x['startlang'];
-    source_start_y[lang] = source_start_y['startlang'];
-    } else if(active_element['targetlang'] == element.id) {
-    source_start_x[lang] = source_start_x['targetlang'];
-    source_start_y[lang] = source_start_y['targetlang'];  
-    } else {
-    source_start_x[lang] = drop_x;
-    source_start_y[lang] = drop_y;
-    }
-    active_element[lang] = element.id;
-    if(lang=='startlang'){
-    drop_x = 400;
-    drop_y = 125;
-    } else {
-    drop_x = 600;
-    drop_y = 125;  
-    }
-    
-
-   
-}
-
-function onHoverAction (element, lang, box) {
- langcode = element.id.split('_');
- if(langcode[1] == $(lang).value) {
-   $(box).setAttribute("class", "reject");   
- }
-}
-
-function onEndAction(element) {
-  new Effect.Move(element.element, { x: drop_x, y: drop_y, mode: 'absolute' });
-  $('inputbox').setAttribute("class", "input-box");  
-  $('outputbox').setAttribute("class", "output-box");  
 }
